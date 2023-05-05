96c24ff1e6f4ef5105a1d66fcbbca284fb4ff396
Bryan Lai <bryanlais@gmail.com>
2023-05-05 13:18:45 +0800

    Use pytest.fixture for setup & teardown in tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,20 +1,36 @@
 import os
 import sys
 
+import pytest
+
 PREFS_PATH_LINUX = os.path.expanduser('~/.config/seatlib/prefs.yml')
 
+
 def rm_prefs():
-    """ clean up previously installed prefs """
+    """ remove installed prefs """
     if sys.platform == 'linux':
         try:
             os.remove(PREFS_PATH_LINUX)
+            print(f"# {PREFS_PATH_LINUX} removed")
         except FileNotFoundError:
+            print(f"# {PREFS_PATH_LINUX} not found")
             pass
 
-def test_prefs():
-    """ check if default prefs are correctly generated """
 
+@pytest.fixture
+def cleanup_prefs():
+    """ clean up before and after the test """
+
+    # before
     rm_prefs()
+    yield
+
+    # after
+    rm_prefs()
+
+
+def test_prefs(cleanup_prefs):
+    """ check if default prefs are correctly generated """
 
     import seatlib  # when imported
     prefs_path = seatlib.find_config(seatlib.PREFS_YML)
