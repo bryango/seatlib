33a179343d97d38b538905f66e83fdc1aefa6aab
Bryan Lai <bryanlais@gmail.com>
2024-01-16 15:57:58 +0800

    Attempt to fix rate limit

--- seatlib.py
+++ seatlib.py
@@ -183,7 +183,26 @@ def load_dataset(
     api_dump_path: str = ''
 ):
     """ load data from API and return a selected subset """
-    with urllib.request.urlopen(api_url) as response:
+    request = urllib.request.Request(
+        api_url,
+        headers={
+            "Host": "seat.lib.tsinghua.edu.cn",
+            "User-Agent": (
+                "Mozilla/5.0 (X11; Linux x86_64; rv:121.0)"
+                " Gecko/20100101"
+                " Firefox/121.0"
+            ),
+            "Accept": "application/json, text/javascript, */*; q=0.01",
+            "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "Cache-Control": "no-cache",
+            "Connection": "keep-alive",
+            "Pragma": "no-cache",
+        }
+    )
+    with urllib.request.urlopen(request) as response:
         data = response.read()
     if api_dump_path:
         with open(api_dump_path, 'wb') as datafile:
